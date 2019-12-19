416c23f5c01d64f287b2c83aa5fbf799b7a5d14b
刘海强 <857523518@qq.com>
2019-12-19 14:54:48 +0800

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -83,13 +83,11 @@ class UsersController extends Controller
     public function sendEmiailConfirmationTo($user){
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '857523518@qq.com';
-        $name = "admin";
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！ 请确认您的邮箱";
 
-        Mail::send($view,$data,function ($message)use($from,$name,$to,$subject){
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view,$data,function ($message)use($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 
